daf0623b6720aba9b2ffb4728beeafaadf2ea0b6
sooqalmelh <54878052+sooqalmelh@users.noreply.github.com>
2025-03-06 02:13:57 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver'                  => 'sqlite',
+            'driver'                  => 'mysql',
             'url'                     => env('DB_URL'),
             'database'                => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix'                  => '',
@@ -47,9 +47,9 @@ return [
             'url'            => env('DB_URL'),
             'host'           => env('DB_HOST', '127.0.0.1'),
             'port'           => env('DB_PORT', '3306'),
-            'database'       => env('DB_DATABASE', 'laravel'),
-            'username'       => env('DB_USERNAME', 'root'),
-            'password'       => env('DB_PASSWORD', ''),
+            'database'       => env('DB_DATABASE', 'db_aureuserp'),
+            'username'       => env('DB_USERNAME', 'admin_aureuserp'),
+            'password'       => env('DB_PASSWORD', 'y88%6Cv8kaureuserp'),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => env('DB_CHARSET', 'utf8mb4'),
             'collation'      => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
